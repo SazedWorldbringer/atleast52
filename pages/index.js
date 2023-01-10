b09ad741fca30cc2b3de335c966bfa851ef5c34f
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-10 23:34:28 +0530

    feat: read and render artwork md files

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/artworks.js'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +25,20 @@ export default function Home() {
           I will post at least one artwork a week, more if I can manage
         </p>
       </section>
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+     <h2 className={utilStyles.headingLg}>Artworks</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+    </section>
     </Layout>
   );
 }
